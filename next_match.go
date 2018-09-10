fddd1d0951240ed773677a5fbbda960881dc45fd
Angad Nadkarni <angad@hullo.ai>
2018-09-10 14:37:09 +0530

    Removed `NextMatch` as it is not a reliable implementation

--- next_match.go
+++ next_match.go
@@ -2,7 +2,6 @@ package cronexpr
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"time"
 )
@@ -15,42 +14,6 @@ var (
 	// satisfy.
 	ErrImpossibleExpression = fmt.Errorf("cron expression is illegal or impossible to satisfy")
 )
-
-// NextMatch finds the closest time instance that is agreeable to all Expressions. All
-// expressions are evaluated in the same time-zone as `fromTime`.
-func NextMatch(fromTime time.Time, expressions ...*Expression) (time.Time, error) {
-	var (
-		n        int
-		nextTime time.Time
-	)
-
-	n = len(expressions)
-	nextTime = fromTime
-
-	for passes := 0; passes <= n; passes++ {
-		maxNextTime := nextTime
-		for i, e := range expressions {
-			// Get the maximal value for the next iteration of nextTime
-			var expTime time.Time
-			if expTime = e.Next(nextTime, NextIfNotMatched); expTime.Equal(time.Time{}) {
-				log.Printf(
-					"expression at index %d is illegal or impossible to satisfy\n", i,
-				)
-				return nextTime, ErrImpossibleExpression
-			}
-
-			maxNextTime = MaxTime(maxNextTime, expTime).In(fromTime.Location())
-		}
-
-		if maxNextTime == nextTime {
-			return nextTime, nil
-		}
-
-		nextTime = maxNextTime
-	}
-
-	return nextTime, ErrNoIntersectionPossible
-}
 
 // NextMatchAny finds the closest time instance that is any of the Expressions. All
 // expressions are evaluated in the same time-zone as `fromTime`.
